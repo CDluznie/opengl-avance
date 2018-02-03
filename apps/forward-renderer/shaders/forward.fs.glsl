a9fdbe1e80725723dc871ecf4664bdb6002ac506
cdluznie <cdluznie@etud.u-pem.fr>
2018-02-03 21:05:47 +0100

    ajout de la lumiere ambiant une unique fois

--- apps/forward-renderer/shaders/forward.fs.glsl
+++ apps/forward-renderer/shaders/forward.fs.glsl
@@ -30,11 +30,10 @@ vec3 blinnPhong(vec3 lightDir, vec3 lightIntensity) {
 	vec3 wi = normalize(lightDir);
 	vec3 w0 = normalize(-vViewSpacePosition);
 	vec3 halfVector = (w0 + wi)/2.;
-	vec3 Ka = 0.15 * texture(uKaSampler, vTexCoords).xyz * uKa;
 	vec3 Kd = texture(uKdSampler, vTexCoords).xyz * uKd;
 	vec3 Ks = texture(uKsSampler, vTexCoords).xyz * uKs;
 	float shininess = texture(uShininessSampler, vTexCoords).x * uShininess;
-	return lightIntensity*(Kd*(max(0., dot(wi,vViewSpaceNormal))) + Ks*(pow(max(0., dot(halfVector,vViewSpaceNormal)), shininess))) + Ka;
+	return lightIntensity*(Kd*(max(0., dot(wi,vViewSpaceNormal))) + Ks*(pow(max(0., dot(halfVector,vViewSpaceNormal)), shininess)));
 }
 
 void main() {
@@ -62,6 +61,10 @@ void main() {
 		CumulatedPointLight += blinnPhong(dirToPointLight, intensityToPointLight);
 	}
 	
-	fColor = CumulatedDirectionalLight + CumulatedPointLight;
+	// Ambiant light add once
+	
+	vec3 Ka = texture(uKaSampler, vTexCoords).xyz * uKa;
+	
+	fColor = CumulatedDirectionalLight + CumulatedPointLight + Ka;
 
 }
